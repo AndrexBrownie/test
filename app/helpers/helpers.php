edd71a5e53e6e2383aad5e709bd7a26af3502d8a
AndrexBrownie <ronal.estradam@gmial.com>
2022-05-16 14:34:30 -0500

    Creación del HostVirtual y Controlador por defecto

---
+++ app/helpers/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+if (! function_exists('myEchoPre')) 
+{
+    function myEchoPre($content)
+    {
+        echo "<pre>", print_r($content), "</pre>";
+    }
+}
+
+if (!function_exists('myEchoDump')) 
+{
+    function myEchoDump($content)
+    {
+        echo "<pre>", var_dump($content), "</pre>";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once "../libs/core.php";
+require_once "../app/helpers/helpers.php";
+
+$Core = new Core();
